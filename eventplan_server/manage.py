c339d2df23ef988219f9e07d6d4ed5b3d6294f5e
szimmerm <szimmerm@akamai.com>
2016-09-15 13:43:50 +0300

    new packages

--- eventplan_server/manage.py
+++ eventplan_server/manage.py
@@ -2,6 +2,7 @@
 import os
 import sys
 
+#TODO: update - this is the default django code
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eventplan_server.settings")
     try:
